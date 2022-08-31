c89ac0811bbac2d1caea42b25d93f1537a9e0e45
Hiroaki Makino <8makino.42tokyo@gmail.com>
2022-09-01 00:04:27 +0900

    update

--- lesson.py
+++ lesson.py
@@ -1,6 +1,11 @@
+# 1num => SyntaxError
+
 num = 0.77
+num2 = 888
 name = 'Mike'
 s = "Hello " + name
 is_ok = True
+num, num2 = num2, num
+num = str(num)
 
-print("Hello world", num, s[:], s[5:10], type(is_ok))
+print("Hello world", name+num, s[:], s[5:10], type(is_ok))
